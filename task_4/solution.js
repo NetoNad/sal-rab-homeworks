cfb34da5a7fe5cdd96e9a247d36b19134c5a3fc2
NadezhdaN <terra666@yandex.ru>
2023-04-10 21:34:21 +0300

    task_4 im 3

--- task_4/solution.js
+++ task_4/solution.js
@@ -8,7 +8,8 @@ function parseProducts(json) {
     // и запишите в переменную data
     // Верните как результат функции свойство products объекта data
     let data = JSON.parse(json);
-    return data.products;
+    let products = data.products;
+    return products;
 }
 
 // Напишите функцию renderProductsCards(json)
